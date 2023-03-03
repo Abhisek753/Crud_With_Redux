c43fa22606eb49d263a75beb3a3dfb863f22c39a
Abhisek753 <properachievement@gmail.com>
2023-03-03 13:16:26 +0530

    book 2

--- book_app/src/Conponents/BookCard.jsx
+++ book_app/src/Conponents/BookCard.jsx
@@ -6,7 +6,7 @@ const BookCard = ({book}) => {
       <img src='https://ttpl.imgix.net/9789391055080L.jpg' alt='book_pic' width="100%"/>
        <h3>{book.book_name}</h3>
        <p>Author:{book.author}</p>
-       <p>Category:{book.caegory}</p>
+       <p>Category:{book.category}</p>
        <p>Year:{book.release_year}</p>
     </div>
   )
--- book_app/src/Conponents/SideBar.jsx
+++ book_app/src/Conponents/SideBar.jsx
@@ -1,25 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const SideBar = () => {
+  const [searchParams,setSearchParams]=useSearchParams()
+  const initialState=searchParams.getAll("category");
+
+  const [category,setCategory]=useState(initialState||[])
+
+
+  console.log(category)
+  const handleFilter=(e)=>{
+   let newCategory=[...category]
+
+    //if a user changes any category it should store in a state
+    // if the category is already present pop out the state 
+
+    if (newCategory.includes(e.target.value)){
+       //filter
+       //splice
+       newCategory.splice(newCategory.indexOf(e.target.value),1)
+    }else{
+      newCategory.push(e.target.value);
+    }
+    setCategory(newCategory);
+    
+  }
+   
+  useEffect(()=>{
+    const params={
+      category
+    }
+    setSearchParams(params);
+  },[category])
+
+
   return (
     <div>
       <h3>Filter By</h3>
       <div>
-        <input type="checkbox"/>
+        <input type="checkbox" value="Novel" onChange={handleFilter} checked={category.includes("Novel")}/>  
         <label>Novel</label>
       </div>
       <div>
-         <input type="checkbox" />
+         <input type="checkbox" value="Motivational" onChange={handleFilter} checked={category.includes("Motivational")}/>
         <label>Motivational</label>
 
       </div>
       <div>
-        <input type="checkbox"/>
+        <input type="checkbox" value="Thriller" onChange={handleFilter} checked={category.includes("Thriller")}/>
         <label>Thriller</label>
 
       </div>
       <div>
-         <input type="checkbox" />
+         <input type="checkbox" value="Science_Fiction" onChange={handleFilter} checked={category.includes("Science_Fiction")}/>
         <label>Science Fiction</label>
 
       </div>
